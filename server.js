6eb6c717f11f0ce0c03a7fa3b92f6c5d090a4408
Anshi Aggarwal <anshi2000aggarwal@gmail.com>
2021-07-21 20:19:08 +0530

    hosting setup

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path=require('path');
 
 const app = express();
 
@@ -11,7 +12,7 @@ app.use(express.json({extended: false}));
 
 //if don't use app.method then it will give an error in body as it serves the end point...
 // app.get('/',(req,res)=> res.send("hello world"));
-app.get('/',(req,res)=> res.json({msg : "welcome to the contact keeper api"}));
+// app.get('/',(req,res)=> res.json({msg : "welcome to the contact keeper api"}));
 
 
 //register access through users.js
@@ -23,7 +24,15 @@ app.use('/api/auth',require('./routes/auth'));
 //contact acess through contact.js
 app.use('/api/contact',require('./routes/contact'));
 
-const PORT = process.env.PORT || 3000;
+//serve static assest in production
+if(process.env.NODE_ENV==='production'){
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res)=>res.sendFile(path.resolve(__dirname,'client','build','index.html')));
+}
+
+const PORT = process.env.PORT || 5000;
 //  const PORT =3000;
 
 app.listen(PORT, ()=> console.log(`server started on port ${PORT}`));
